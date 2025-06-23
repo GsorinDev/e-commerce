d0b35f0d45cfdc5223e8a56f799857eb554c82ba
Gsorin <georgio.sorin@gmail.com>
2025-06-23 08:48:07 +0200

    initial commit

---
+++ front/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: '../styles.css'
+})
+export class App {
+  protected title = 'e-commerce-front';
+}
